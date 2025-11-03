18fdfdbd033a5dd7ed783581deeadaf548f6e395
oystub <o_github@oystub.com>
2025-11-03 20:10:49 +0100

    disable position limits by default

--- fw/bldc_servo_structs.h
+++ fw/bldc_servo_structs.h
@@ -672,8 +672,8 @@ struct BldcServoConfig {
 // separate to minimize resets due to schemas changing during
 // development.
 struct BldcServoPositionConfig {
-  float position_min = -0.01f;
-  float position_max = 0.01f;
+  float position_min = std::numeric_limits<float>::quiet_NaN();
+  float position_max = std::numeric_limits<float>::quiet_NaN();
 
   template <typename Archive>
   void Serialize(Archive* a) {
